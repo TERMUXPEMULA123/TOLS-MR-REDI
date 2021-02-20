cd6a5aaee836838515dc5895d8152d433e14a9ae
TERMUXPEMULA123 <79355260+TERMUXPEMULA123@users.noreply.github.com>
2021-02-21 01:52:52 +0700

    Add files via upload

---
+++ MR-REDI.sh
@@ -0,0 +1,72 @@
+clear
+sleep 1
+echo
+toilet -f big -F gay By Mr.Redi
+echo $green"selamat mencoba"
+echo
+echo $red"01.)TOLS TUANB4DUT"
+echo
+echo $red"02.)TOLS B4J1N64Nv5"
+echo
+echo $blue"03.)TOLS Mr-R225 DAN Mr.Rv1.1"
+echo
+echo $blue"04.)kembali aja"
+echo
+echo $white
+read -p "PilihMana : " bro
+
+if [ $bro = 01 ] || [ $bro = 01 ]
+then
+clear
+toilet -f big -F Rd tutorial
+echo $green"TOLS TUANB4DUT"
+sleep 1
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+
+if [ $bro = 02 ] || [ $bro = 02 ]
+then
+clear
+toilet -f big -F gay Rd Tutorial
+echo $green"TOLS B4J1N64Nv5"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install gem
+pkg install figlet
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+gem install lolcat
+sh B4J1N64N.sh
+fi
+
+if [ $bro = 03 ] || [ $bro = 03 ]
+then
+clear
+toilet -f big -F gay Rd Tutorial
+echo $green"TOLS Mr-R225 DAN Mr.Rv1.1"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install gem
+pkg install figlet
+gem install lolcat
+git clone https://github.com/Mr-R225/Mr.Rv1.1
+cd Mr.Rv1.1
+sh Mr.Rv1.1.sh
+fi
+
+if [ $bro = 04 ] || [ $bro = 04 ]
+then
+clear
+echo $yellow"kembali aja"
+figlet "Bey"
+echo $yellow"To Be Continued"
+sleep 1
+Exit
+fi
+
+
